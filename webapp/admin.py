8597f231f83bded4eb1881645b89049247f986d5
captainQ <lordkuvin@gmail.com>
2022-02-05 12:05:45 +0600

    Зарегистрировал модели в админ-панели и добавил тестовые данные

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(Choice)
--- webapp/models.py
+++ webapp/models.py
@@ -15,17 +15,17 @@ class Poll(BaseModel):
         return f'{self.id}. {self.question}'
 
     class Meta:
-        db_table = 'Pools'
+        db_table = 'Polls'
         verbose_name = 'Опрос'
         verbose_name_plural = 'Опросы'
 
 
 class Choice(models.Model):
     option = models.TextField(max_length=200, verbose_name='Текст варианта')
-    pool = models.ForeignKey('webapp.Poll',
+    poll = models.ForeignKey('webapp.Poll',
                              on_delete=models.CASCADE,
                              verbose_name='Опрос',
-                             related_name='pool')
+                             related_name='poll')
 
     def __str__(self):
         return f'{self.id}. {self.option}'
