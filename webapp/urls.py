c2eb4597803044a34f67b3c39f67c92131fe3da3
captainQ <lordkuvin@gmail.com>
2022-02-05 12:55:09 +0600

    Добавлен создание опроса

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from views import PollIndexView, PollDetailView
+from views import PollIndexView, PollDetailView, PollCreateView
 
 urlpatterns = [
     path('', PollIndexView.as_view(), name="poll_index"),
-    path('poll/<int:pk>/', PollDetailView.as_view(), name='poll_view')
+    path('poll/<int:pk>/', PollDetailView.as_view(), name='poll_view'),
+    path('poll/add/', PollCreateView.as_view(), name='poll_create'),
 ]
--- webapp/views/polls.py
+++ webapp/views/polls.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from webapp.forms import PollForm
 from webapp.models import Poll
 
 
@@ -16,4 +18,13 @@ class PollIndexView(ListView):
 class PollDetailView(DetailView):
     model = Poll
     template_name = 'polls/view.html'
+
+
+class PollCreateView(CreateView):
+    model = Poll
+    form_class = PollForm
+    template_name = 'polls/create.html'
+
+    def get_success_url(self):
+        return reverse('poll_view', kwargs={'pk': self.object.pk})
 
