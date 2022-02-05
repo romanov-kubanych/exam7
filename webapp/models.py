6568479961d845aeeafa9eb6f5602e58ada2b7f4
captainQ <lordkuvin@gmail.com>
2022-02-05 11:45:22 +0600

    Создана базовая модель BaseModel Создана модель Poll(Опрос)

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    class Meta:
+        abstract = True
+
+
+class Poll(BaseModel):
+    question = models.TextField(max_length=500, verbose_name='Вопрос')
+
+    def __str__(self):
+        return f'{self.id}. {self.question}'
+
+    class Meta:
+        db_table = 'Pools'
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
